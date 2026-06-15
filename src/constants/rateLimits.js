f1a8902fb6fbb41d4da80d3821305c83e2710fb4
PerspicaciousGuy <joy.0839b@gmail.com>
2026-06-15 21:05:30 +0530

    feat: add rate limits and premium access tiers constants

---
+++ src/constants/rateLimits.js
@@ -0,0 +1,8 @@
+export const DAILY_REQUEST_LIMITS_BY_TIER = {
+  free: 1000,
+  basic: 10000,
+  pro: 100000,
+  enterprise: 1000000
+};
+
+export const PREMIUM_ACCESS_TIERS = new Set(['pro', 'enterprise']);
