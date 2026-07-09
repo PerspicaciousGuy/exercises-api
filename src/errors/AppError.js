0d2fd7b6ec9173919732b5e4770252c519ab62d3
PerspicaciousGuy <joy.0839b@gmail.com>
2026-07-09 10:45:45 +0530

    feat: add retryAfterSeconds parameter to AppError constructor

--- src/errors/AppError.js
+++ src/errors/AppError.js
@@ -1,7 +1,8 @@
 export class AppError extends Error {
-  constructor({ statusCode, code, message }) {
+  constructor({ statusCode, code, message, retryAfterSeconds }) {
     super(message);
     this.statusCode = statusCode;
     this.code = code;
+    this.retryAfterSeconds = retryAfterSeconds;
   }
 }
